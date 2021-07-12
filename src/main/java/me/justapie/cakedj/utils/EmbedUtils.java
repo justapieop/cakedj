8e065f440f9435e4a1c255efe6143767ac07c76f
Do Tran Thai An <justapie06@gmail.com>
2021-07-12 10:48:55 +0700

    Add EmbedUtils to send embeds easily

---
+++ src/main/java/me/justapie/cakedj/utils/EmbedUtils.java
@@ -0,0 +1,14 @@
+package me.justapie.cakedj.utils;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+
+import java.awt.*;
+
+public class EmbedUtils {
+    public static void sendEmbed(SlashCommandEvent event, Color color, String desc) {
+        event.deferReply().addEmbeds(
+                new EmbedBuilder().setColor(color).setDescription(desc).build()
+        ).setEphemeral(true).queue();
+    }
+}
