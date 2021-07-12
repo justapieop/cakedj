6c9d543e8baf4267ea18b4795418279bdd165dda
Do Tran Thai An <justapie06@gmail.com>
2021-07-12 21:29:24 +0700

    Update listener

--- src/main/java/me/justapie/cakedj/Listener.java
+++ src/main/java/me/justapie/cakedj/Listener.java
@@ -2,9 +2,12 @@ package me.justapie.cakedj;
 
 import me.justapie.cakedj.command.Manager;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.StageChannel;
 import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.managers.AudioManager;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,5 +30,19 @@ public class Listener extends ListenerAdapter {
     public void onSlashCommand(@NotNull SlashCommandEvent event) {
         super.onSlashCommand(event);
         man.process(event);
+    }
+
+    @Override
+    public void onGuildVoiceJoin(@NotNull GuildVoiceJoinEvent event) {
+        super.onGuildVoiceJoin(event);
+        if (event.getChannelJoined() instanceof StageChannel) {
+            StageChannel stage = (StageChannel) event.getChannelJoined();
+            if (stage.getStageInstance() == null) stage.createStageInstance(Constants.musicWithCakeDJ).queue();
+            event.getGuild().requestToSpeak();
+            return;
+        }
+        AudioManager audioMan = event.getGuild().getAudioManager();
+        audioMan.setSelfMuted(false);
+        audioMan.setSelfDeafened(true);
     }
 }
