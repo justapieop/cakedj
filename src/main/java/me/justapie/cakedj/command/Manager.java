1f0d1c20d99ba9e4ff2b6d9f7d09653a41ccd700
Do Tran Thai An <justapie06@gmail.com>
2021-07-15 10:15:16 +0700

    Added command preprocess

--- src/main/java/me/justapie/cakedj/command/Manager.java
+++ src/main/java/me/justapie/cakedj/command/Manager.java
@@ -1,8 +1,12 @@
 package me.justapie.cakedj.command;
 
 import me.justapie.cakedj.Constants;
+import me.justapie.cakedj.audio.GuildMusicManager;
+import me.justapie.cakedj.audio.PlayerManager;
 import me.justapie.cakedj.command.commands.music.*;
+import me.justapie.cakedj.database.collections.ConfigCollection;
 import me.justapie.cakedj.utils.EmbedUtils;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 
@@ -43,11 +47,81 @@ public class Manager {
 
     public void process(SlashCommandEvent event) {
         ICommand command = this.getCommand(event.getName());
+        if (this.preprocess(event, command)) command.exec(event);
+    }
+
+    private boolean preprocess(SlashCommandEvent event, ICommand command) {
         if (command == null) {
             EmbedUtils.sendEmbed(event, Color.RED, Constants.noCommandFound);
-            return;
+            return false;
         }
 
-        command.exec(event);
+        if (command.customDefined()) return true;
+
+        String ownerID = ConfigCollection.getConfig().ownerID();
+
+        if (command.isOwnerCommand()) {
+            if (!event.getUser().getId().equals(ownerID)) {
+                EmbedUtils.sendEmbed(event, Color.RED, Constants.ownerRequired);
+                return false;
+            }
+        }
+
+        if (!event.getMember().hasPermission(command.getUserPermissions())) {
+            EmbedUtils.sendEmbed(event, Color.RED, Constants.noPerms);
+            return false;
+        }
+
+        if (!event.getGuild().getSelfMember().hasPermission(command.getBotPermission())) {
+            EmbedUtils.sendEmbed(event, Color.RED, Constants.noBotPerms);
+            return false;
+        }
+
+        GuildVoiceState botState = event.getGuild().getSelfMember().getVoiceState();
+        GuildVoiceState userState = event.getMember().getVoiceState();
+
+        if (command.needConnectedVoice()) {
+            if (botState == null || userState == null) {
+                EmbedUtils.sendEmbed(event, Color.RED, Constants.cannotDetectState);
+                return false;
+            }
+            if (!userState.inVoiceChannel()) {
+                EmbedUtils.sendEmbed(event, Color.RED, Constants.cannotDetectState);
+                return false;
+            }
+        }
+
+        if (command.sameVoice()) {
+            if (botState == null || userState == null) {
+                EmbedUtils.sendEmbed(event, Color.RED, Constants.cannotDetectState);
+                return false;
+            }
+            if (!userState.inVoiceChannel()) {
+                EmbedUtils.sendEmbed(event, Color.RED, Constants.cannotDetectState);
+                return false;
+            }
+            if (botState.getChannel() != userState.getChannel()) {
+                EmbedUtils.sendEmbed(event, Color.RED, Constants.noSameVoice);
+                return false;
+            }
+        }
+
+        GuildMusicManager musicMan = PlayerManager.getInstance().getMusicManager(event.getGuild());
+
+        if (command.activePlayer()) {
+            if (musicMan.audioPlayer.getPlayingTrack() == null) {
+                EmbedUtils.sendEmbed(event, Color.RED, Constants.noActivePlayer);
+                return false;
+            }
+        }
+
+        if (command.activeQueue()) {
+            if (musicMan.scheduler.queue.isEmpty()) {
+                EmbedUtils.sendEmbed(event, Color.RED, Constants.noActiveQueue);
+                return false;
+            }
+        }
+
+        return true;
     }
 }
