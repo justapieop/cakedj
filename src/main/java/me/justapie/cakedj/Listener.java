f129ed7187888a8d173b649150f1f1c1c3df2000
Do Tran Thai An <justapie06@gmail.com>
2021-07-15 10:14:54 +0700

    Listen to more events...

--- src/main/java/me/justapie/cakedj/Listener.java
+++ src/main/java/me/justapie/cakedj/Listener.java
@@ -1,16 +1,28 @@
 package me.justapie.cakedj;
 
+import me.justapie.cakedj.audio.GuildMusicManager;
+import me.justapie.cakedj.audio.PlayerManager;
 import me.justapie.cakedj.command.Manager;
+import me.justapie.cakedj.database.collections.GuildCollection;
+import me.justapie.cakedj.database.models.GuildModel;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.StageChannel;
 import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.channel.text.TextChannelDeleteEvent;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.managers.AudioManager;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Listener extends ListenerAdapter {
     private static final Logger log = LoggerFactory.getLogger(Listener.class);
@@ -30,6 +42,11 @@ public class Listener extends ListenerAdapter {
     public void onSlashCommand(@NotNull SlashCommandEvent event) {
         super.onSlashCommand(event);
         if (event.getGuild() == null) return;
+        GuildModel guildSetting = GuildCollection.getGuildConfig(event.getGuild());
+        if (guildSetting == null) return;
+        if (guildSetting.channelRestrict())
+            if (!guildSetting.djOnlyChannels().contains(event.getChannel().getId()))
+                return;
         man.process(event);
     }
 
@@ -45,5 +62,50 @@ public class Listener extends ListenerAdapter {
         AudioManager audioMan = event.getGuild().getAudioManager();
         audioMan.setSelfMuted(false);
         audioMan.setSelfDeafened(true);
+    }
+
+    @Override
+    public void onGuildVoiceLeave(@NotNull GuildVoiceLeaveEvent event) {
+        super.onGuildVoiceLeave(event);
+        GuildModel guildSetting = GuildCollection.getGuildConfig(event.getGuild());
+        assert guildSetting != null;
+        if (guildSetting.is247()) return;
+        GuildMusicManager musicMan = PlayerManager.getInstance().getMusicManager(event.getGuild());
+        if (event.getMember().equals(event.getGuild().getSelfMember())) {
+            musicMan.audioPlayer.setFilterFactory(null);
+            GuildCollection.modifyGuildConfig(event.getGuild(), Constants.is247Key, false);
+            musicMan.audioPlayer.destroy();
+        } else {
+            List<Member> members = event.getChannelLeft().getMembers();
+            members = members.stream().filter(
+                    (member) -> !member.getUser().isBot()
+            ).collect(Collectors.toList());
+            if (members.isEmpty()) {
+                musicMan.audioPlayer.destroy();
+                event.getGuild().getAudioManager().closeAudioConnection();
+            }
+        }
+    }
+
+    @Override
+    public void onTextChannelDelete(@NotNull TextChannelDeleteEvent event) {
+        super.onTextChannelDelete(event);
+        GuildModel guildSetting = GuildCollection.getGuildConfig(event.getGuild());
+        assert guildSetting != null;
+        List<String> djOnlyChannels = guildSetting.djOnlyChannels();
+        djOnlyChannels.remove(event.getChannel().getId());
+        GuildCollection.modifyGuildConfig(event.getGuild(), Constants.djOnlyChannelsKey, djOnlyChannels);
+    }
+
+    @Override
+    public void onGuildLeave(@NotNull GuildLeaveEvent event) {
+        super.onGuildLeave(event);
+        GuildCollection.deleteGuildConfig(event.getGuild());
+    }
+
+    @Override
+    public void onGuildJoin(@NotNull GuildJoinEvent event) {
+        super.onGuildJoin(event);
+        GuildCollection.createGuildConfig(event.getGuild());
     }
 }
