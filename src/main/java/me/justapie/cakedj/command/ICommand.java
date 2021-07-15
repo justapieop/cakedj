260c1b6ab919ff5973192c2dd9ebbc3ac4fb7943
Do Tran Thai An <justapie06@gmail.com>
2021-07-15 10:16:54 +0700

    Format code

--- src/main/java/me/justapie/cakedj/command/ICommand.java
+++ src/main/java/me/justapie/cakedj/command/ICommand.java
@@ -8,13 +8,33 @@ import java.util.List;
 
 public interface ICommand {
     void exec(SlashCommandEvent event);
+
     CommandData getCommandData();
-    default List<Permission> getUserPermissions() { return List.of(); }
-    default List<Permission> getBotPermission() { return List.of(); }
-    default boolean isOwnerCommand() { return false; }
-    default boolean needConnectedVoice() { return false; }
-    default boolean sameVoice() { return false; }
-    default boolean activeQueue() { return false; }
+
+    default List<Permission> getUserPermissions() {
+        return List.of();
+    }
+
+    default List<Permission> getBotPermission() {
+        return List.of();
+    }
+
+    default boolean isOwnerCommand() {
+        return false;
+    }
+
+    default boolean needConnectedVoice() {
+        return false;
+    }
+
+    default boolean sameVoice() {
+        return false;
+    }
+
+    default boolean activeQueue() {
+        return false;
+    }
+
     default boolean activePlayer() {
         return false;
     }
