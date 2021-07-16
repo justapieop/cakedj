597e9698610029f35dd58e102faf5031dc3060c4
Do Tran Thai An <justapie06@gmail.com>
2021-07-16 23:04:33 +0700

    Remove Owner in BotInfo

--- src/main/java/me/justapie/cakedj/command/commands/info/BotInfoCommand.java
+++ src/main/java/me/justapie/cakedj/command/commands/info/BotInfoCommand.java
@@ -1,7 +1,6 @@
 package me.justapie.cakedj.command.commands.info;
 
 import me.justapie.cakedj.command.ICommand;
-import me.justapie.cakedj.database.collections.ConfigCollection;
 import me.justapie.cakedj.utils.EmbedUtils;
 import me.justapie.cakedj.utils.TimeUtils;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -18,7 +17,6 @@ public class BotInfoCommand implements ICommand {
                 .setThumbnail(self.getAvatarUrl())
                 .addField("OS", System.getProperty("os.name"), true)
                 .addField("Java version", System.getProperty("java.version"), true)
-                .addField("Owner", event.getJDA().getUserById(ConfigCollection.getConfig().ownerID()).getAsTag(), true)
                 .addField("Bot created in", TimeUtils.formatDate(self.getTimeCreated()), true)
                 .addField("Total servers", String.valueOf(event.getJDA().getGuilds().size()), true)
                 .addField("Total shards", String.valueOf(event.getJDA().getShardManager().getShardsTotal()), true);
