33f06b72e796a8ec4f2ce411dd9b486b13e2c629
JustAPie <justapie06@gmail.com>
2022-01-10 21:53:08 +0700

    Rename methods and usage

--- src/main/java/me/justapie/cakedj/database/DatabaseUtils.java
+++ src/main/java/me/justapie/cakedj/database/DatabaseUtils.java
@@ -4,36 +4,30 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
-import me.justapie.cakedj.database.model.GuildSetting;
-import net.dv8tion.jda.api.entities.Guild;
 import org.bson.Document;
 
 public final class DatabaseUtils {
     private static final MongoClient CLIENT = MongoDBHandler.getClient();
     private static final MongoDatabase DATABASE = CLIENT.getDatabase("CakeDJ");
 
-    public static Document getDocument(Guild guild) {
-        MongoCollection<Document> collection = DATABASE.getCollection("guilds");
-        return collection.find(Filters.eq("guildId", guild.getId())).first();
+    public static Document getDocument(String collName, String key, Object val) {
+        MongoCollection<Document> collection = DATABASE.getCollection(collName);
+        return collection.find(Filters.eq(key, val)).first();
     }
 
-    public static void updateData(String guildId, Document setting) {
-        MongoCollection<Document> collection = DATABASE.getCollection("guilds");
-        deleteSetting(guildId);
+    public static void updateDocument(String collName, String key, Object val, Document setting) {
+        MongoCollection<Document> collection = DATABASE.getCollection(collName);
+        deleteDocument(collName, key, val);
         collection.insertOne(setting);
     }
 
-    public static void createSetting(String guildId, String guildName) {
-        MongoCollection<Document> collection = DATABASE.getCollection("guilds");
-        collection.insertOne(
-                new Document()
-                        .append("guildId", guildId)
-                        .append("guildName", guildName)
-        );
+    public static void createDocument(String collName, Document doc) {
+        MongoCollection<Document> collection = DATABASE.getCollection(collName);
+        collection.insertOne(doc);
     }
 
-    public static void deleteSetting(String guildId) {
-        MongoCollection<GuildSetting> collection = DATABASE.getCollection("guilds", GuildSetting.class);
-        collection.findOneAndDelete(Filters.eq("guildId", guildId));
+    public static void deleteDocument(String collName, String key, Object val) {
+        MongoCollection<Document> collection = DATABASE.getCollection(collName);
+        collection.deleteMany(Filters.eq(key, val));
     }
 }
