6715dfb57019e29cd03af78b3d43589d56aefa13
JustAPie <justapie06@gmail.com>
2022-01-10 21:52:51 +0700

    Add more event listeners

--- src/main/java/me/justapie/cakedj/Listener.java
+++ src/main/java/me/justapie/cakedj/Listener.java
@@ -1,14 +1,22 @@
 package me.justapie.cakedj;
 
+import me.justapie.cakedj.audio.GuildMusicManager;
+import me.justapie.cakedj.audio.PlayerManager;
+import me.justapie.cakedj.database.DatabaseUtils;
 import me.justapie.cakedj.database.MongoDBHandler;
+import net.dv8tion.jda.api.entities.AudioChannel;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.StageChannel;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.ShutdownEvent;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.managers.AudioManager;
+import org.bson.Document;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,6 +44,41 @@ public final class Listener extends ListenerAdapter {
     public void onSlashCommand(@NotNull SlashCommandEvent event) {
         super.onSlashCommand(event);
         COMMAND_MANAGER.process(event);
+    }
+
+    @Override
+    public void onGuildJoin(@NotNull GuildJoinEvent event) {
+        super.onGuildJoin(event);
+        DatabaseUtils.createDocument("guilds", new Document()
+                .append("guildID", event.getGuild().getId())
+                .append("guildName", event.getGuild().getName())
+        );
+    }
+
+    @Override
+    public void onGuildLeave(@NotNull GuildLeaveEvent event) {
+        super.onGuildLeave(event);
+        DatabaseUtils.deleteDocument("guilds", "guildID", event.getGuild().getId());
+    }
+
+    @Override
+    public void onGuildVoiceLeave(@NotNull GuildVoiceLeaveEvent event) {
+        super.onGuildVoiceLeave(event);
+        AudioChannel old = event.getChannelLeft();
+        if (!old.getMembers().contains(event.getGuild().getSelfMember())) return;
+        long numOfMembers = old.getMembers().stream().filter(
+                (c) -> !c.getUser().isBot()
+        ).count();
+        if (numOfMembers != 0) return;
+        Document doc = DatabaseUtils.getDocument("guilds", "guildID", event.getGuild().getId());
+        AudioManager audioMan = event.getGuild().getAudioManager();
+        GuildMusicManager musicMan = PlayerManager.getInstance().getMusicManager(event.getGuild());
+        if (doc.getBoolean("is247")) {
+            if (!musicMan.scheduler.getQueueData().getQueue().isEmpty()) return;
+            audioMan.closeAudioConnection();
+            return;
+        }
+        audioMan.closeAudioConnection();
     }
 
     @Override
