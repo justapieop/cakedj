591682319886a22c700825d7916cd9e0892c8ad2
JustAPie <justapie06@gmail.com>
2022-01-13 11:43:23 +0700

    Added force option

--- src/main/java/me/justapie/cakedj/command/music/PlayCommand.java
+++ src/main/java/me/justapie/cakedj/command/music/PlayCommand.java
@@ -1,7 +1,10 @@
 package me.justapie.cakedj.command.music;
 
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import me.justapie.cakedj.Context;
+import me.justapie.cakedj.audio.GuildMusicManager;
 import me.justapie.cakedj.audio.PlayerManager;
+import me.justapie.cakedj.audio.QueueData;
 import me.justapie.cakedj.structure.Command;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
@@ -23,6 +26,7 @@ public final class PlayCommand extends Command {
         this.sameVoice = true;
         this.userPerms = List.of(Permission.VOICE_CONNECT);
         this.botPerms = List.of(Permission.VOICE_CONNECT, Permission.VOICE_SPEAK, Permission.REQUEST_TO_SPEAK);
+        this.djPerm = true;
     }
 
     @Override
@@ -44,5 +48,13 @@ public final class PlayCommand extends Command {
             songName = "scsearch:" + songName;
         }
         PlayerManager.getInstance().loadAndPlay(context, context.getInteraction().getUser(), songName);
+        if (context.getOptions().size() == 2 && context.getOptions().get(1).getAsBoolean()) {
+            GuildMusicManager musicMan = context.getMusicManager();
+            AudioTrack playing = musicMan.audioPlayer.getPlayingTrack();
+            QueueData data = musicMan.scheduler.getQueueData();
+            if (data.getQueue().isEmpty()) return;
+            data.getQueue().addFirst(playing.makeClone());
+            musicMan.audioPlayer.startTrack(data.getQueue().remove(1), false);
+        }
     }
 }
